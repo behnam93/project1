fc89434a9829803a26d0ad723c25d03943e1627b
Behnam <behnams_id@yahoo.com>
2020-02-26 12:24:45 +0330

    introduce ViewSets and learn router urls and add list, create, retrive, update, partial_update, destroy to views.py

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register('hello-veiwset', views.HelloViewSet,
+                basename='hello-veiwset')
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,51 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test Api View Set """
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update , partial_update)',
+            'Automatically maps to URLs using Router',
+            'Provides more functionality with less code',
+        ]
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrivee(self, request, pk=None):
+        """ handle getting an object by its id """
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """ handle updating an object """
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """ handle updating part of object """
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """ handle removing an object by its id """
+        return Response({'http_method': 'DELETE'})
 
 
 class HelloApiView(APIView):
